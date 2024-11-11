cb88bfd6730d4f6dcbcb298d52532bad68cd0459
Adeel Qamar <adeel.qamar@emea.merkleinc.com>
2024-11-11 15:11:41 +0100

    Added UserController

---
+++ src/main/java/com/vestas/libraryManagement/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.vestas.libraryManagement.controllers;
+
+import com.vestas.libraryManagement.entities.User;
+import com.vestas.libraryManagement.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository myUserRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register/user")
+    public User createUser(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return myUserRepository.save(user);
+    }
+}
